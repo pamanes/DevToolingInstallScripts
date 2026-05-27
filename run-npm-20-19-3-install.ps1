2a68138b7a6fd20058be0a717f7030234ca79762
Alejandro Palacios <pamanes7@hotmail.com>
2026-05-27 10:19:47 -0700

    Add PowerShell script for installing Node.js 20.19.3

---
+++ run-npm-20-19-3-install.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = "Stop" 
+# Set active path to script-location:
+$path = $MyInvocation.MyCommand.Path
+if (!$path) {$path = $psISE.CurrentFile.Fullpath}
+if ($path)  {$path = Split-Path $path -Parent}
+Set-Location $path
+
+. ".\install_functions.ps1"
+
+
+WingetInstall "OpenJS.NodeJS" -e -v 20.19.3
+Read-Host -Prompt "Done." | Out-Null
